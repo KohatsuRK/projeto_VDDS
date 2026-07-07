e14cb474d09b1cba0816ab91cd4934679e6a2c09
DanteYTsubono <dante.tsubono@unifesp.br>
2026-07-06 22:28:14 -0300

    Teste e2e - Chrome driver manager byee

--- backend/tests/test_13_e2e.py
+++ backend/tests/test_13_e2e.py
@@ -27,8 +27,7 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     driver.get(STREAMLIT_URL)
 
     # Espera o Streamlit carregar completamente
